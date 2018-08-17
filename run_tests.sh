381801b0345ca7866b9abf56e232d0547ca144ef
Gregory Izatt <gregory.izatt@gmail.com>
2018-08-17 16:27:44 -0400

    Test flipping sim too

--- run_tests.sh
+++ run_tests.sh
@@ -14,3 +14,11 @@ if [ ! $exit_status -eq 0 ]; then
   echo "Error code in kuka_pydrake_sim.py: " $exit_status
   exit $exit_status
 fi
+
+# Launch a complete robot context and execute some canned movement.
+DISPLAY=:100 python kuka_flipping_sim.py -T 5. --test
+exit_status=$?
+if [ ! $exit_status -eq 0 ]; then
+  echo "Error code in kuka_pydrake_sim.py: " $exit_status
+  exit $exit_status
+fi
